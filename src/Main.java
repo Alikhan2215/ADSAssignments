9962ebeda71dde259c9ed1e744f15eff1acf9b26
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-07 20:51:05 +0600

    finding minimum number from array with documentation

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,23 @@ public class Main {
             arr[i] = scanner.nextInt();
         }
         int min = findMinFromArr(arr, n);
-
-        System.out.println(min);
+        System.out.println("1 Problem. Min from arr is: " + min);
     }
 
+    /*
+    int findMinFromArr(int[] arr, int n)
+
+    @findMinFromArr - function for finding minimum number from array
+    @parameter  int[] arr - arr from which we find minimum number
+    @parameter int n - number of elements in array
+    @return - minimum number from array
+    */
     public static int findMinFromArr(int[] arr, int n){
         if(n==1)
             return arr[0];
         else {
-            int min = findMinFromArr(arr, n - 1);
-            return min < arr[n-1]?min:arr[n-1];
-            //return Math.min(min, arr[n-1]);
+            int min = findMinFromArr(arr, n - 1); //move with n-1 length
+            return Math.min(min, arr[n-1]);//return lesser number
         }
     }
 }
