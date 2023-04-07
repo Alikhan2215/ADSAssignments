5ae9bce0d5678a60d069c7d556a526a9b31edb42
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-07 20:55:20 +0600

    solution to the second problem - finding average number from array

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,10 @@ public class Main {
         }
         int min = findMinFromArr(arr, n);
         System.out.println("1 Problem. Min from arr is: " + min);
+
+        double avg = findAvg(arr, n);
+        System.out.println("2 Problem. Average from arr is: " + avg);
+
     }
 
     /*
@@ -29,4 +33,23 @@ public class Main {
             return Math.min(min, arr[n-1]);//return lesser number
         }
     }
+
+
+        /*
+    double findAvg(int[] arr, int n)
+
+    @findAvg - function for finding average number from array
+    @parameter int[] arr - array from which we find average number
+    @parameter int n - number of elements in array
+    @return - average number from array
+    */
+        public static double findAvg(int[] arr, int n){
+            double sum = 0;
+            for(int i = 0; i < n; i++){ //creating a loop to sum all numbers in array
+                sum = sum + arr[i];
+            }
+            double avg = sum / n; //defining average
+            return avg;
+        }
+
 }
